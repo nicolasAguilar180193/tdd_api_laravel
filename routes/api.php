56272f8313e571f692764ab5a74abd9724142100
Nicolas <nicolas.aguilar180193@gmail.com>
2024-01-29 14:11:04 -0300

    Obtener todos los articulos

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\Api\ArticleController;
 
 
 Route::get('articles/{article}', [ArticleController::class, 'show'])->name('api.v1.articles.show');
+
+Route::get('articles', [ArticleController::class, 'index'])->name('api.v1.articles.index');
--- tests/Feature/Articles/ListArticlesTest.php
+++ tests/Feature/Articles/ListArticlesTest.php
@@ -19,7 +19,7 @@ class ListArticlesTest extends TestCase
 
         $article = Article::factory()->create();
 
-        $response = $this->getJson(route('api.v1.articles.show', $article))->dump();
+        $response = $this->getJson(route('api.v1.articles.show', $article));
         
         $response->assertExactJson([
             'data' => [
@@ -36,4 +36,57 @@ class ListArticlesTest extends TestCase
             ]
         ]);
     }
+
+    public function test_can_fetch_all_articles(): void
+    {
+        $this->withoutExceptionHandling();
+        $articles = Article::factory()->count(3)->create();
+
+        $response = $this->getJson(route('api.v1.articles.index'));
+
+        $response->assertExactJson([
+            'data' => [
+                [
+                    'type' => 'articles',
+                    'id' => (string) $articles[0]->getRouteKey(),
+                    'attributes' => [
+                        'title' => $articles[0]->title,
+                        'slug' => $articles[0]->slug,
+                        'content' => $articles[0]->content
+                    ],
+                    'link' => [
+                        'self' => route('api.v1.articles.show', $articles[0])
+                    ]
+                ],
+                [
+                    'type' => 'articles',
+                    'id' => (string) $articles[1]->getRouteKey(),
+                    'attributes' => [
+                        'title' => $articles[1]->title,
+                        'slug' => $articles[1]->slug,
+                        'content' => $articles[1]->content
+                    ],
+                    'link' => [
+                        'self' => route('api.v1.articles.show', $articles[1])
+                    ]
+                ],
+                [
+                    'type' => 'articles',
+                    'id' => (string) $articles[2]->getRouteKey(),
+                    'attributes' => [
+                        'title' => $articles[2]->title,
+                        'slug' => $articles[2]->slug,
+                        'content' => $articles[2]->content
+                    ],
+                    'link' => [
+                        'self' => route('api.v1.articles.show', $articles[2])
+                    ]
+                ]
+            ],
+            'links' => [
+                'self' => route('api.v1.articles.index')
+            ]
+        ]);
+    }
+
 }
